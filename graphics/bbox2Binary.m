7b7acd57b1b7c736167d7209a017bb9ee037ac1b
Stan Bileschi <sbileschi@dataxu.com>
2012-01-19 01:49:56 -0500

    moved bbox2Binary.m

--- graphics/bbox2Binary.m
+++ graphics/bbox2Binary.m
@@ -1,6 +1,9 @@
 function bin = bbox2Binary(bbox,imsize)
 %function bin = bbox2Binary(bbox,imsize)
 %
+% creates an image of all zeros, of size imsize.
+% points inside the bounding box bbox will be ones.
+% bbox is stored as [x, y, width, height]
 z = zeros(imsize(1:2));
 bbox = BBoxIntersect(bbox, [1, 1, imsize(2)-1, imsize(1)-1]);
 z(bbox(2):(bbox(2)+bbox(4)),bbox(1):(bbox(1)+bbox(3))) = 1;
