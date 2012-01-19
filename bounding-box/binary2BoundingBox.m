75cfa23a6ffe9c0520c570082abeab435a02f186
Stan Bileschi <sbileschi@dataxu.com>
2012-01-19 02:16:16 -0500

    created directories for AdaptiveHistogram and filters stuff

--- bounding-box/binary2BoundingBox.m
+++ bounding-box/binary2BoundingBox.m
@@ -3,6 +3,8 @@ function [bbox,boxIsEmpty] = binary2BoundingBox(bw1)
 %
 %returns a the minimum box containing the selected region in bw1
 %
+% this is the smallest box such that it contains all non-zero pixels in bw1
+% bounding boxes are encoded as [x, y, width, height]
 boxIsEmpty = 0;
 if(not(any(bw1(:))))
   boxIsEmpty = 1;
